c173e90f3639bc3842aa5f3fa18539301ad7996e
Koala999 <8396825+koala999@user.noreply.gitee.com>
2021-10-06 08:15:37 +0000

    update src/test/test_util.cpp.

--- src/test/test_util.cpp
+++ src/test/test_util.cpp
@@ -3,23 +3,6 @@
 #include <string>
 
 
-
-// 返回[x0, x1]区间的随机数
-double rand(double x0, double x1)
-{
-    return x0 + (x1 - x0) * rand() / RAND_MAX;
-}
-
-
-// 以prob的概率返回true.
-// 0 <= prob <= 1
-bool rand_p(double prob)
-{
-    auto r = rand();
-    return r <= RAND_MAX * prob;
-}
-
-
 bool almostEqual(double x1, double x2, double tol)
 {
     double dif = x1 - x2;
