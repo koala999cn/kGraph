b9e4242a4f580abfb2cff457ca5e83606ea5acc6
koala999 <koala999@aliyun.com>
2022-04-11 19:02:40 +0800

    add: HAS_OVERLOADED_BIOP

--- src/base/traits_helper.h
+++ src/base/traits_helper.h
@@ -25,3 +25,20 @@
     public: \
         static constexpr auto value = decltype(Check<T>(0))::value; \
     };
+
+
+// 用于判定两个类型是否具有重载的op操作符
+#define HAS_OVERLOADED_BIOP(op, name) \
+template<typename T1, typename T2> \
+struct has_operator_##name \
+{ \
+private: \
+    template<typename U1, typename U2> static auto Check(int) -> \
+        decltype(std::declval<U1>() op std::declval<U2>(), std::true_type()); \
+    template<typename U1, typename U2> static std::false_type Check(...); \
+public: \
+    static constexpr auto value = decltype(Check<T1, T2>(0))::value; \
+};
+
+HAS_OVERLOADED_BIOP(<< , output)
+HAS_OVERLOADED_BIOP(>> , input)
