033bad48b9dcd2cee02425685560a82989cda851
koala999 <koala999@aliyun.com>
2022-04-21 12:09:59 +0800

    refactor: 优化整型特化

--- src/core/vertex_traits.h
+++ src/core/vertex_traits.h
@@ -20,79 +20,18 @@ struct vertex_traits
 	//static auto weight(VERTEX_TYPE);
 
 	//static auto edgeindex(VERTEX_TYPE); 仅对flat图有效
-};
 
+	template<typename T = vertex_type, 
+		typename = std::enable_if_t<std::is_integral_v<T>>>
+	static T edgeindex(const T& v) { 
+		return v; 
+	}
 
-
-template<>
-struct vertex_traits<std::int32_t>
-{
-	using vertex_type = std::int32_t;
-
-	static vertex_type edgeindex(const vertex_type& v) { return v; }
-	static vertex_type& edgeindex(vertex_type& v) { return v; }
-};
-
-template<>
-struct vertex_traits<const std::int32_t>
-{
-	using vertex_type = const std::int32_t;
-
-	static std::int32_t edgeindex(vertex_type v) { return v; }
-};
-
-
-template<>
-struct vertex_traits<std::uint32_t>
-{
-	using vertex_type = std::uint32_t;
-
-	static vertex_type edgeindex(const vertex_type& v) { return v; }
-	static vertex_type& edgeindex(vertex_type& v) { return v; }
-};
-
-template<>
-struct vertex_traits<const std::uint32_t>
-{
-	using vertex_type = const std::uint32_t;
-
-	static std::uint32_t edgeindex(vertex_type v) { return v; }
-};
-
-
-template<>
-struct vertex_traits<std::int64_t>
-{
-	using vertex_type = std::int64_t;
-
-	static vertex_type edgeindex(const vertex_type& v) { return v; }
-	static vertex_type& edgeindex(vertex_type& v) { return v; }
-};
-
-template<>
-struct vertex_traits<const std::int64_t>
-{
-	using vertex_type = const std::int64_t;
-
-	static std::int64_t edgeindex(vertex_type v) { return v; }
-};
-
-
-template<>
-struct vertex_traits<std::uint64_t>
-{
-	using vertex_type = std::uint64_t;
-
-	static vertex_type edgeindex(const vertex_type& v) { return v; }
-	static vertex_type& edgeindex(vertex_type& v) { return v; }
-};
-
-template<>
-struct vertex_traits<const std::uint64_t>
-{
-	using vertex_type = const std::uint64_t;
-
-	static std::uint64_t edgeindex(vertex_type v) { return v; }
+	template<typename T = vertex_type, 
+		typename = std::enable_if_t<std::is_integral_v<T> && !std::is_const_v<T>>>
+	static T& edgeindex(T& v) { 
+		return v; 
+	}
 };
 
 
@@ -148,6 +87,7 @@ namespace kPrivate
 	HAS_STATIC_MEMBER(outdegree);
 	HAS_STATIC_MEMBER(outedges);
 	HAS_STATIC_MEMBER(inedges);
+	HAS_STATIC_MEMBER(weight);
 }
 
 
@@ -161,6 +101,7 @@ struct vertex_traits_helper
 	constexpr static bool has_outdegree = kPrivate::has_static_member_outdegree<VERTEX_TRAITS, vertex_type>::value;
 	constexpr static bool has_outedges = kPrivate::has_static_member_outedges<VERTEX_TRAITS, vertex_type>::value;
 	constexpr static bool has_inedges = kPrivate::has_static_member_inedges<VERTEX_TRAITS, vertex_type>::value;
+	constexpr static bool has_weight = kPrivate::has_static_member_weight<VERTEX_TRAITS, vertex_type>::value;
 };
 
 
@@ -175,6 +116,11 @@ namespace kPrivate
 	constexpr static bool hasOutDegree_() {
 		return vertex_traits_helper<vertex_traits<VERTEX_TYPE>>::has_outdegree;
 	}
+
+	template<typename VERTEX_TYPE>
+	constexpr static bool hasWeight_() {
+		return vertex_traits_helper<vertex_traits<VERTEX_TYPE>>::has_weight;
+	}
 }
 
 
@@ -183,6 +129,9 @@ inline constexpr bool has_edgeindex_v = kPrivate::hasEdgeIndex_<VERTEX_TYPE>();
 
 template<typename VERTEX_TYPE>
 inline constexpr bool has_outdegree_v = kPrivate::hasOutDegree_<VERTEX_TYPE>();
+
+template<typename VERTEX_TYPE>
+inline constexpr bool has_weight_v = kPrivate::hasWeight_<VERTEX_TYPE>();
 
 
 // 若VERTEX_TYPE不带边偏移信息，使用KtFlatVertexWrapper_附加一个
