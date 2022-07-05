9a6e66ea0b9dd43a1c700cc7d988b64043420423
koala999 <koala999@aliyun.com>
2022-07-05 15:49:56 +0800

    refactor: support copy from digraph/graph to graph/digraph

--- graph/util/copy.h
+++ graph/util/copy.h
@@ -3,30 +3,38 @@
 #include "../core/KtWeightor.h"
 
 
-template<typename SRC_G, typename DST_G, 
-    typename WEIGHTOR = KtWeightSelf<typename SRC_G::edge_type>>
-DST_G copy(const SRC_G& src)
+template<typename SRC, typename DST, typename WTOR = KtWeightSelf<typename SRC::edge_type>>
+DST copy(const SRC& src)
 {
-    DST_G dst;
+    DST dst;
     dst.reserve(src.order(), src.size());
     dst.reset(src.order()); 
     for (unsigned v = 0; v < src.order(); v++) {
-        unsigned wMax = dst.isDigraph() ? unsigned(src.order()) : v + 1;
+        unsigned wMax = src.isDigraph() ? unsigned(src.order()) : v + 1;
         for (unsigned w = 0; w < wMax; w++)
             for (auto r = src.edges(v, w); !r.empty(); ++r)
-                dst.addEdge(v, w, WEIGHTOR{}(*r));
+                if constexpr (std::is_same_v<typename DST::edge_type, bool>) {
+                    dst.addEdge(v, w);
+                    if constexpr (DST::isDigraph() && !SRC::isDigraph()) // 无向图转有向图，加双边
+                        dst.addEdge(w, v);
+                }
+                else {
+                    dst.addEdge(v, w, WTOR{}(*r));
+                    if constexpr (DST::isDigraph() && !SRC::isDigraph()) // 无向图转有向图，加双边
+                        dst.addEdge(w, v, WTOR{}(*r));
+                }
 
-        if constexpr (SRC_G::hasVertex() && DST_G::hasVertex())
+        if constexpr (SRC::hasVertex() && DST::hasVertex())
             dst.vertexAt(v) = src.vertexAt(v);
     }
 
+    assert(src.size() == dst.size());
     return dst;
 }
 
 
-template<typename SRC_G, typename DST_G,
-    typename WEIGHTOR = KtWeightSelf<typename SRC_G::edge_type>>
-void copy(const SRC_G& src, DST_G& dst)
+template<typename SRC, typename DST, typename WTOR = KtWeightSelf<typename SRC::edge_type>>
+void copy(const SRC& src, DST& dst)
 {
-    dst = copy<SRC_G, DST_G, WEIGHTOR>(src);
+    dst = copy<SRC, DST, WTOR>(src);
 }
