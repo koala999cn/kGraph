f8d8672c2013185454d96533c893d80670a37d5a
koala999 <koala999@aliyun.com>
2022-07-06 08:58:18 +0800

    refactor: use another version of degree

--- graph/util/minmax_degree.h
+++ graph/util/minmax_degree.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include "degree.h"
 
 
 // 计算图g的最大最小度
@@ -8,7 +9,7 @@ std::pair<unsigned, unsigned> minmax_degree(const GRAPH& g)
 {
 	unsigned maxdeg = 0, mindeg = std::numeric_limits<unsigned>::max();
 	for (unsigned v = 0; v < g.order(); v++) {
-		auto vdeg = g.degree(v);
+		auto vdeg = degree(g, v);
 		if (vdeg > maxdeg)
 			maxdeg = vdeg;
 		if (vdeg < mindeg)
